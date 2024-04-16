7ceb59cf88b288c89bfd40e20e4e44a2ab66934e
wolffanil2 <wolffanil@mail.ru>
2024-04-17 00:02:31 +0400

    fix message

--- server/services/messageService.js
+++ server/services/messageService.js
@@ -1,4 +1,4 @@
-const Message = require("../models/MessageModel");
+const Message = require("../models/messageModel");
 const Chat = require("../models/chatModel");
 const AppError = require("../utils/AppError");
 
